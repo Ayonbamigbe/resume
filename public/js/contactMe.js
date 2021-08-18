be4de66947222452a4427109f8529efffdd9e8f8
Daramola Ayobami <ayox2012@gmail.com>
2021-08-19 00:57:11 +0100

    first commit

---
+++ public/js/contactMe.js
@@ -0,0 +1,27 @@
+const form = document.getElementById('contact-form');
+const url = "http://localhost:5000/send";
+
+const formEvent = form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    let mail = new FormData(form);
+    sendMail(mail);
+});
+
+const sendMail = (mail) => {
+    fetch(url, {
+        method: "post",
+        body:mail,
+    }).then((response) => {
+        return response.json();
+    });
+};
+
+
+
+
+
+
+
+
+
